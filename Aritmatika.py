67c39575608c28920f4cad9e0ef7a633d306b6ae
Maarnaaa <marnaarn@gmail.com>
2025-11-07 22:56:27 +0800

    Add files via upload

---
+++ Aritmatika.py
@@ -0,0 +1,27 @@
+#Operasi Penjumlahan
+g = 4
+h = 6
+i = 9
+total = g + h + i
+print(f"Hasil penjumlahan {g} + {h} + {i} adalah: {total}")
+
+#Operasi Pengurangan
+a = 15
+b = 4
+c = 6
+hasil_kurang = a - b - c
+print(f"Hasil pengurangan {a} - {b} - {c} adalah: {hasil_kurang}")
+
+#Operasi Perkalian
+p = 2
+q = 5
+r = 8
+hasil_kali = p * q * r
+print(f"Hasil perkalian {p} * {q} * {r} adalah: {hasil_kali}")
+
+#Operasi Pembagian
+m = 36
+n = 3
+o = 2
+hasil_bagi = m / n / o
+print(f"Hasil pembagian {m} / {n} / {o} adalah: {hasil_bagi}")
